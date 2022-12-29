5487f43dbf2ee61ed8a4771438d14cb7354694f5
YayCover9000 <yassuanf@googlemail.com>
2022-12-29 19:31:30 +0100

    Added Iterative Methode

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,30 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Scanner sc = new Scanner(System.in);
-        int ausgebe50 = 0;
-        ausgebe50 = FibonacciZahlenBis50Ausgeben.main(ausgebe50);
-        System.out.println("Bitte Arrayposition Rekursiv die angezeigt werden soll eingeben: ");
-        long ausgabeAuswahl = sc.nextLong();
-        ausgabeAuswahl = RecursiveFibonacciSequence.main(ausgabeAuswahl);
-        System.out.println("Bitte Arrayposition Iterativ die angezeigt werden soll eingeben: ");
-        int ausgabeIterativ = sc.nextInt();
+        System.out.println("Enter a number to choose Methodes: ");
+        int choose = sc.nextInt();
+        if (choose > 4) {
+            System.out.println("Diese Methode existiert nicht! /t Bitte wählen Sie eine andere Methode mithilfe einer Zahl!");
+        } else {
+            switch (choose) {
+                case 1 :
+                    int ausgebe50 = 0;
+                    ausgebe50 = FibonacciZahlenBis50Ausgeben.main(ausgebe50);
+                    break;
+                case 2 :
+                    System.out.println("Bitte Arrayposition Rekursiv die angezeigt werden soll eingeben: ");
+                    long ausgabeAuswahl = sc.nextLong();
+                    ausgabeAuswahl = RecursiveFibonacciSequence.mainR(ausgabeAuswahl);
+                    break;
+                case 3 :
+                    System.out.println("Bitte Arrayposition Iterativ die angezeigt werden soll eingeben: ");
+                    int ausgabeIterativ = sc.nextInt();
+                    ausgabeIterativ = RecursiveFibonacciSequence.getFibonacciNumbersAtI(ausgabeIterativ);
+                    break;
+                default:
+                    System.out.println("Diese Methode existiert nicht! /t Bitte wählen Sie eine andere Methode mithilfe einer Zahl!");
+                    break;
+            }
+        }
     }
 }
--- src/RecursiveFibonacciSequence.java
+++ src/RecursiveFibonacciSequence.java
@@ -1,5 +1,5 @@
 public class RecursiveFibonacciSequence {
-    public static long main(long ausgabeAuswahl) {
+    public static long mainR(long ausgabeAuswahl) {
         long x = getFibonacciNumbersAt(ausgabeAuswahl);
         System.out.println(x);
         return x;
@@ -11,7 +11,19 @@ public class RecursiveFibonacciSequence {
             return getFibonacciNumbersAt(n - 1) + getFibonacciNumbersAt(n - 2);
         }
     }
-    public static int main(I){
-
+    public static int mainI(int ausgabeIterativ) {
+        int x = getFibonacciNumbersAtI(ausgabeIterativ);
+        System.out.println(x);
+        return x;
+    }
+    public static int getFibonacciNumbersAtI(int n) {
+        int last = 0;
+        int next = 1;
+        for (int i = 0; i < n; i++) {
+            int old_last = last;
+            last = next;
+            next = old_last + next;
+        }
+        return next;
     }
 }
